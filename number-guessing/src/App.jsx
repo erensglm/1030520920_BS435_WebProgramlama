7d7cf6956ec284eb1ceb8d45ab8ec4e860b913fd
jovekola <erensglm@gmail.com>
2023-12-11 20:32:19 +0300

    .

--- number-guessing/src/App.jsx
+++ number-guessing/src/App.jsx
@@ -1,13 +1,15 @@
 import './style.css';
-import GameOne from "./GameOne";
 import {useNavigate} from "react-router-dom";
 import React from "react";
+import {Button, Col, Row} from "react-bootstrap";
 
 function App() {
     const navigate = useNavigate()
   return (
     <div className="App">
         <h2>SAYI TAHMİN OYUNU</h2>
+        <> <Row>
+            <Col>
         <p className="welcome-text">
             Budala Mezarlığına hoşgeldin evlat. Ben X.
             Durmaksızın aylaklık yaptığın için internetin en ücra köşesine geldin ve benim sınırlarım içerisine girdin.
@@ -17,11 +19,14 @@ function App() {
             Fakat oyunları oynayıp kolayca kurtulabileceğini sanıyorsan adım X kadar eminim ki budalasın demektir ve burda kalmayı sonuna hak ediyorsun.
             Bu oyunları kazanmak asla kolay olmayacak ama bu demek değildir ki X alçakgönüllü değildir.
             Bunun bir göstergesi olarak oynamak istediğin oyunu seçmene izin vereceğim. Eğer hazırsan başlayalım değilsen de başlamak zorundasın çünkü X seni sabaha kadar bekleyemez.
-
         </p>
+            </Col>
+            <Col>
         <button onClick={()=>navigate("GameOne")}> Oyun 1</button>
         <button onClick={()=>navigate("GameTwo")}> Oyun 2</button>
-
+            </Col>
+            </Row>
+        </>
     </div>
   );
 
