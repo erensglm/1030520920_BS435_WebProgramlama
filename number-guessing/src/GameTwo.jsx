c9179bc33435c3c0b9db0b3e5337980641cd1fc5
jovekola <erensglm@gmail.com>
2023-12-13 00:48:15 +0300

    updatesforg2

--- number-guessing/src/GameTwo.jsx
+++ number-guessing/src/GameTwo.jsx
@@ -1,53 +1,100 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function getRandom(max) {
-    return Math.floor(Math.random() * max);
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getMessage(guess, rnd) {
+function getMessage(guess, target) {
     const guessNo = Number(guess);
-    if (guessNo < rnd) return "Bilemedin ahmak daha yüksek bir tahminde bulun.";
-    if (guessNo > rnd) return "Fazla uçuyorsun daha alçak bir tahmin yap.";
-    if (guessNo === rnd) return "Nasıl olur! Kahretsin, bunu bilmeni beklemiyordum.";
+    if (guessNo < target) return "Bilemedin ahmak daha yüksek bir tahminde bulun.";
+    if (guessNo > target) return "Fazla uçuyorsun daha alçak bir tahmin yap.";
+    if (guessNo === target) return "Kahretsin! Kiddo'yu kendi oyununda alt ettin. Seni özgür bırakacağım ama burdan kaçtın diye benden kurtulduğunu sanıyorsan yanılıyorsun! ";
 }
 
 function GameTwo() {
+    const [min, setMin] = useState(1);
+    const [max, setMax] = useState(100);
     const [guess, setGuess] = useState("");
-    const [rnd, setRnd] = useState(getRandom(100));
+    const [target, setTarget] = useState(null);
     const [msg, setMsg] = useState("");
     const [count, setCount] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
-    const checkGuess = () => {
-        setMsg(getMessage(guess, rnd));
-        setCount((count) => count + 1);
-    };
-
-    const start = () => {
-        setRnd(getRandom(100));
+    const startGame = () => {
+        setTarget(getRandom(min, max));
         setCount(0);
         setMsg("");
+        setGameOver(false);
+    };
+
+    const activateGame = () => {
+        if (min < max) {
+            startGame();
+        } else {
+            setMsg("Lütfen geçerli bir sayı aralığı belirtin.");
+        }
+    };
+
+    const checkGuess = () => {
+        const guessNo = Number(guess);
+        if (count < 2 && !gameOver) {
+            setCount((count) => count + 1);
+            setMsg(getMessage(guess, target));
+            if (guessNo === target) {
+                setGameOver(true);
+            }
+        } else {
+            setMsg("Ahmak, benim oyunumda beni yeneceğini mi sanıyordun? ");
+            setGameOver(true);
+        }
     };
 
     return (
         <form>
-            <input
-                type="number"
-                value={guess}
-                onChange={(e) => setGuess(e.target.value)}
-            />
-            {msg !== "Great!" ? (
-                <button type="button" onClick={checkGuess}>
-                    Tahminini görelim evlat.
-                </button>
-            ) : (
-                <button type="button" onClick={start}>
-                    Eğer cesaretin varsa bir daha oynarsın!
-                </button>
+            <label>
+                Min Sayı:
+                <input
+                    type="number"
+                    value={min}
+                    onChange={(e) => setMin(Number(e.target.value))}
+                />
+            </label>
+            <label>
+                Max Sayı:
+                <input
+                    type="number"
+                    value={max}
+                    onChange={(e) => setMax(Number(e.target.value))}
+                />
+            </label>
+            <button type="button" onClick={activateGame}>
+                Seni mahkum edecek sayıları seçtiysen başlayalım!
+            </button>
+            <br />
+            {target !== null && (
+                <>
+                    <input
+                        type="number"
+                        value={guess}
+                        onChange={(e) => setGuess(e.target.value)}
+                    />
+                    {gameOver ? (
+                        <button type="button" onClick={startGame}>
+                            Benim oyunumu tekrar tekrar oynayabilirsin ama her kaybettiğinde klavyendeki bir tuşu bozacağım!
+                        </button>
+                    ) : (
+                        <>
+                            <button type="button" onClick={checkGuess}>
+                                Tahminini görelim evlat.
+                            </button>
+                            <div>{count} kere denedin seni hapsetmeme az kaldı.</div>
+                        </>
+                    )}
+                    <div>{msg}</div>
+                </>
             )}
-            <div>{msg}</div>
-            <div>{count} kere denedin üstüne toprak atmama ramak kaldı. </div>
         </form>
     );
-
 }
+
 export default GameTwo;
